bc6a1a0b1c1f85472abfcd384b6afcb98122c243
DanielHdez <dhdezleon@gmail.com>
2023-01-21 10:57:05 +0000

    Finalizando Listas

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -46,8 +46,47 @@ sap.ui.define([
                     }
 
                     sap.m.MessageToast.show(i18nModel.getText(textMessage))
-
                 }
+            },
+            onDeleteSelectedRow: function(){
+                var standardList = this.getView().byId("standardList");
+                var selectedItems = standardList.getSelectedItems();
+                var i18nModel = this.getView().getModel("i18n").getResourceBundle();
+                if ( selectedItems.length === 0){
+                    sap.m.MessageToast.show(i18nModel.getText("no_seleccion"))
+
+                }else{
+                    var textMessage = i18nModel.getText("seleccion");
+                    var arrayid = [];
+                    var model = this.getView().getModel();
+                    var products = model.getProperty("/Products");
+                    for( var item in selectedItems){
+                        var context=selectedItems[item].getBindingContext();
+                        var ocontext = context.getObject();
+                        arrayid.push(ocontext.Id);
+                        textMessage = textMessage + "-" + ocontext.Material;
+                    }
+
+                    products = products.filter(function(p){
+                         return !arrayid.includes(p.Id);   
+                    });
+                    model.setProperty("/Products", products);
+                    standardList.removeSelections();
+                    sap.m.MessageToast.show(i18nModel.getText(textMessage))
+                }
+            },
+
+            onDeleteRow: function(oEvent){
+                var selectedRowoEvent = oEvent.getParameter("listItem");
+                var context = selectedRowoEvent.getBindingContext();
+                var splitPath = context.getPath().split("/");
+                var splitPath = context.getPath().split("/");
+                var indexSelectedRow = splitPath[splitPath.length-1];
+                var model = this.getView().getModel();
+                var products = model.getProperty("/Products");
+                products.splice(indexSelectedRow,1);
+                model.refresh();
+
             }
         });
     });
